285bf5903223a2bd80aa7c635d9f0a5f7c5ce4bd
SarfarajSalimKhan <sarfaraz.salimkhan17@rediffmail.com>
2019-10-08 17:35:27 +0530

    Added first method

---
+++ method1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void insertArray(int [], int);
+void printArray(int [], int);
+void rotateArray(int [], int, int);
+
+int main()
+{
+    const int size = 7;
+    int arr[size];
+    insertArray(arr, size);
+    printArray(arr, size);
+    int num;
+    cout << endl << "Enter number of elements to rotate : ";
+    cin >> num;
+    rotateArray(arr, size, num);
+    printArray(arr, size);
+    return 0;
+}
+
+void insertArray(int arr[], int size)
+{
+    cout << "Enter elements of array" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    cout << "Array is :";
+    for (int i = 0; i < size; i++)
+    {
+        cout << "\t" << arr[i];
+    }
+}
+
+void rotateArray(int arr[], int size, int num)
+{
+    int temp[num];
+    for (int i = 0; i < num; i++)
+    {
+        temp[i] = arr[i];
+    }
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = arr[i + num];
+    }
+    int count = 0;
+    for (int i = size - num; i < size; i++)
+    {
+        count;
+        arr[i] = temp[count];
+        count++;
+    }
+}
